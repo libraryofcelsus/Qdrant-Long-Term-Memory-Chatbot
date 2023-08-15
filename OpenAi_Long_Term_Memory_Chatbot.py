8d2982b3c6ba4d14a6432c85c07fc2401575eccc
Library of Celsus <127127527+libraryofcelsus@users.noreply.github.com>
2023-08-15 10:19:33 -0700

    Add files via upload

--- OpenAi_Long_Term_Memory_Chatbot.py
+++ OpenAi_Long_Term_Memory_Chatbot.py
@@ -1,15 +1,43 @@
-import requests
-import os
 import time
+import os
 import json
-from time import time
+from time import time, sleep
 from datetime import datetime
 from uuid import uuid4
+import openai
+import requests
 from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams, PointStruct, Filter, FieldCondition, Range, MatchValue
 from qdrant_client.http import models
 from sentence_transformers import SentenceTransformer
 import re
+        
+        
+with open('key_openai.txt', 'r', encoding='utf-8') as file:
+    openai.api_key = file.read().strip()
+
+
+# Function for generating a reply form the OpenAi Api
+def chatgpt_completion(query):
+    max_counter = 7
+    counter = 0
+    while True:
+        try:
+            completion = openai.ChatCompletion.create(
+              model="gpt-3.5-turbo",
+              max_tokens=800,
+              temperature=0.5,
+              messages=query
+            )
+            response = (completion.choices[0].message.content)
+            return response
+        except Exception as e:
+            counter +=1
+            if counter >= max_counter:
+                print(f"Exiting with error: {e}")
+                exit()
+            print(f"Retrying with error: {e} in 20 seconds...")
+            sleep(20)
         
         
 def open_file(filepath):
@@ -23,13 +51,6 @@ def timestamp_to_datetime(unix_time):
     return datetime_str
 
 
-# Connect to Oobabooga Api
-# For local streaming, the websockets are hosted without ssl - http://
-HOST = 'localhost:5000'
-URI = f'http://{HOST}/api/v1/chat'
-
-# For reverse-proxied streaming, the remote will likely host with ssl - https://
-# URI = 'https://your-uri-here.trycloudflare.com/api/v1/generate'
 
 model = SentenceTransformer('all-mpnet-base-v2')
 
@@ -54,62 +75,6 @@ else:
         print("Connected to cloud Qdrant server.")
     except Exception as e:
         print(f"Failed to Connect to Qdrant Server: {e}")
-    
-
-
-def oobabooga(instruction, prompt):
-    history = {'internal': [], 'visible': []}
-    request = {
-        'user_input': prompt,
-        'max_new_tokens': 800,
-        'history': history,
-        'mode': 'instruct',  # Valid options: 'chat', 'chat-instruct', 'instruct'
-        'instruction_template': 'Llama-v2',  # Will get autodetected if unset
-        'context_instruct': f"{instruction}",  # Optional
-        'your_name': f'USER',
-        'regenerate': False,
-        '_continue': False,
-        'stop_at_newline': False,
-        'chat_generation_attempts': 1,
-        # Generation params. If 'preset' is set to different than 'None', the values
-        # in presets/preset-name.yaml are used instead of the individual numbers.
-        'preset': 'None',  
-        'do_sample': True,
-        'temperature': 0.85,
-        'top_p': 0.55,
-        'typical_p': 1,
-        'epsilon_cutoff': 0,  # In units of 1e-4
-        'eta_cutoff': 0,  # In units of 1e-4
-        'tfs': 1,
-        'top_a': 0,
-        'repetition_penalty': 1.18,
-        'top_k': 35,
-        'min_length': 100,
-        'no_repeat_ngram_size': 0,
-        'num_beams': 1,
-        'penalty_alpha': 0,
-        'length_penalty': 1,
-        'early_stopping': False,
-        'mirostat_mode': 0,
-        'mirostat_tau': 5,
-        'mirostat_eta': 0.1,
-
-        'seed': -1,
-        'add_bos_token': True,
-        'truncation_length': 4096,
-        'ban_eos_token': False,
-        'skip_special_tokens': True,
-        'stopping_strings': []
-    }
-
-    response = requests.post(URI, json=request)
-
-    if response.status_code == 200:
-        result = response.json()['results'][0]['history']
-    #    print(json.dumps(result, indent=4))
-        print()
-    #    print(result['visible'][-1][1])
-        return result['visible'][-1][1]
         
         
 def Qdrant_Upload(bot_name, query):
@@ -174,7 +139,7 @@ class MainConversation:
     def append(self, usernameupper, user_input, botnameupper, output):
         # Append new entry to the running conversation
         entry = []
-        entry.append(f"[INST] {usernameupper}: {user_input} [/INST]")
+        entry.append(f"{usernameupper}: {user_input}")
         entry.append(f"{botnameupper}: {output}")
         self.running_conversation.append("\n\n".join(entry))  # Join the entry with "\n\n"
         # Remove oldest entry if conversation length exceeds max entries
@@ -205,9 +170,7 @@ if __name__ == '__main__':
     conversation = list()
     summary = list()
     bot_name = open_file('./Prompts/bot_name.txt')
-    user_name = open_file('./Prompts/bot_name.txt')
     botnameupper = bot_name.upper()
-    usernameupper = user_name.upper()
     main_prompt = open_file(f'./Prompts/prompt_main.txt').replace('<<NAME>>', bot_name)
     greeting_prompt = open_file(f'./Prompts/prompt_greeting.txt').replace('<<NAME>>', bot_name)
     collection_name = f"ENTER COLLECTION NAME HERE"
@@ -218,7 +181,6 @@ if __name__ == '__main__':
     while True:
         try:
             conversation_history = main_conversation.get_conversation_history()
-            instruction = "[INST] <<SYS>>\nYou are in the middle of a conversation with a user, generate a natural sounding response to their message.\n<</SYS>>"
             user_input = input(f'\n\nUSER: ')
             db_result = None
             try:
@@ -248,19 +210,16 @@ if __name__ == '__main__':
                     print("Collection has no memories.")
                 else:
                     print(f"An unexpected error occurred: {str(e)}")
-        #    print(db_result)
-            conversation.append({'content': f"{botnameupper}'S LONG TERM MEMORY: {db_result}[/INST]"})
-            conversation.append({'content': f"CURRENT CONVERSATION HISTORY: {conversation_history}"})
-            conversation.append({'content': f"[INST] USER INPUT: {user_input} [/INST]"})    
-            conversation.append({'content': f"{botnameupper}: "})    
-            prompt = ''.join([message_dict['content'] for message_dict in conversation])
-            output = oobabooga(instruction, prompt)
+            conversation.append({'role': 'system', 'content': f"{main_prompt}"})
+            conversation.append({'role': 'assistant', 'content': f"{botnameupper}'S LONG TERM MEMORIES: {db_result}"})
+            conversation.append({'role': 'assistant', 'content': f"CURRENT CONVERSATION HISTORY: {conversation_history}"})
+            conversation.append({'role': 'user', 'content': user_input}) 
+            output = chatgpt_completion(conversation)
             print(f"{botnameupper}: {output}")
-            main_conversation.append(usernameupper, user_input, botnameupper, output)
-            instruction = f"[INST] <<SYS>>\nExtract short and concise memories based on {bot_name}'s final response for upload to a memory database.  These should be executive summaries and will serve as {bot_name}'s memories.  Use the bullet point format: •<Executive Summary>\n<</SYS>>"
-            summary.append({'content': f"LOG: {output}[/INST][INST]SYSTEM: Use the log to extract the salient points about the user and {bot_name}'s conversation. These points should be used to create concise executive summaries in bullet point format to serve as {bot_name}'s memories. Each bullet point should be considered a separate memory and contain full context.  Use the bullet point format: •<Executive Summary>[/INST]{botnameupper}: Sure! Here are some memories based on {bot_name}'s response:"})
-            prompt = ''.join([message_dict['content'] for message_dict in summary])
-            output_sum = oobabooga(instruction, prompt)
+            summary.append({'role': 'system', 'content': f"You are a summarizer submodule for {bot_name}.  Your purpose is to extract short and concise memories based on {bot_name}'s final response for upload to a memory database. These should be executive summaries and contain full context.  Use the bullet point format: •<Executive Summary>"})
+            summary.append({'role': 'assistant', 'content': f"[USER INPUT: {user_input}\n{botnameupper}'S RESPONSE: {output}]"})
+            summary.append({'role': 'user', 'content': f"Please extract the salient points and generate memories for {bot_name} in the following bullet point format: •<Executive Summary>"})
+            output_sum = chatgpt_completion(summary)
             print(output_sum)
             mem_check = input(f'\n\nUpload Memories? Y or N?: ')
             if 'y' in mem_check.lower():
